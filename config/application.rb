304c47b2aac349713c20cef58111c3e8e385d3e2
Paulo Rodrigues <paulor2222@gmail.com>
2019-11-16 00:04:26 -0200

    configured test env

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module Affinity
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
+    end
   end
 end
